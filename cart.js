ee0b722ee090b0eb31545adaec04061a938a971d
Steve Witman <stevewitman@gmail.com>
2016-11-14 23:55:06 -0700

    refactor change function

--- cart.js
+++ cart.js
@@ -1,5 +1,8 @@
 var cart = [];
-cart = [['12ft HDMI Cable', 6.76], ['JavaScript For Kids', 15.37]]
+cart = [['12ft HDMI Cable', 676], ['JavaScript For Kids', 1537]];
+var amountDue = total(cart);
+var amountPaid = 4000;
+change(amountDue, amountPaid);
 
 function total(cart) {
 	var result = 0
@@ -11,4 +14,31 @@ function total(cart) {
 	return result;
 }
 
-total(cart);
+function change(total, cash) {
+	var money = [
+		['twenties', 2000],
+		['tens', 1000],
+		['fives', 500],
+		['ones', 100],
+		['quarters', 25],
+		['dimes', 10],
+		['nickles', 5],
+		['pennies', 1]
+	];
+	var change = [];
+	var diff = cash - total;
+
+	for (var i=0; i < money.length; i++) {
+		var denomination = money[i];
+		var count = 0;
+		while (diff >= denomination[1]) {
+			count++;
+			diff = diff - denomination[1];
+		}
+		if (count) {
+			console.log(count + ' ' + denomination[0])
+		}
+
+	}
+
+}
